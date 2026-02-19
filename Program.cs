2d31ee03d2c0c00c031fe642e1f6ede3f9174056
ialikh72 <ialikh.w1@gmail.com>
2026-02-19 23:25:53 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Are you Muslim(Y/N)");
+        string religion = Console.ReadLine();
+        Console.WriteLine("Enter your Current Balance:");
+        double balance = Convert.ToDouble(Console.ReadLine());
+        if(religion == "N")
+            Console.WriteLine("Zakat is not applicable to you.....");
+        else
+        {
+            if(balance >= 500000)
+            {
+                double zakat = balance * 0.025;
+                Console.WriteLine("Your Zakat amount is: " + zakat);
+            }
+            else
+                Console.WriteLine("Zakat is not applicable to you due to low balance....");
+        }
+    }
+}
